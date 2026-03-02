4d840d94389ead6f0747b3d1bec2a8b351e25aeb
salvat <tobias@avlas.cz>
2026-03-02 15:36:37 +0100

    Play around with hw0

--- hw/salva0/app/Main.hs
+++ hw/salva0/app/Main.hs
@@ -1,4 +1,22 @@
 module Main where
+import Data.Char (toLower, isAlpha)
+
+vowels :: String
+vowels = "aeiouy"
+
+isVowel :: Char -> Bool
+isVowel c = c `elem` vowels
+
+countVowels :: String -> Int
+countVowels = length . filter isVowel
+
+computeVowelPercentage :: String -> Int
+computeVowelPercentage w = (countVowels w * 100) `div` length w
+
+collectLetters :: String -> String
+collectLetters str = concat $ ( filter ((>= 3) . length) . words)
+        $ filter isAlpha $ fmap toLower str
+
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = interact $ (++"%") . show . computeVowelPercentage . collectLetters
