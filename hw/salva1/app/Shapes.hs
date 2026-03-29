7d8d56e0e810913ddf2dcdc8e6611cfdd68433ec
salvat <tobias@avlas.cz>
2026-03-29 11:39:04 +0200

    Implement a Parallelogram Shape

--- hw/salva1/app/Shapes.hs
+++ hw/salva1/app/Shapes.hs
@@ -10,7 +10,9 @@ type IntPoint = (Int, Int)
 data Shape
   = Square IntPoint Int Color
   | Triangle IntPoint IntPoint IntPoint Color
+  | Parallel IntPoint IntPoint Int Color
 
+  
 -- My coordinates are in Ints,
 -- (0,0) starts at top left corner instead of middle
 -- And the unit is defined as a size of a small square, that is relative to the window size
@@ -19,15 +21,21 @@ translateCoordinates (x, y) =
   ( fromIntegral $ (x * unitSize) - windowWidth `div` 2
   , fromIntegral $ windowHeight `div` 2 - (y * unitSize))
 
+(x,y) `addC` a = (x+a,y+a)
+
 draw :: Shape -> Picture
 draw (Square (x, y) size clr) =
   Color clr
     $ Polygon
     $ translateCoordinates
         <$> [(x, y), (x + size, y), (x + size, y + size), (x, y + size)]
-        
+
 draw (Triangle a b c clr) =
   Color clr $ Polygon $ translateCoordinates <$> [a, b, c]
+
+
+draw (Parallel (x1,y1) (x2,y2) len clr) =
+  Color clr $ Polygon $ translateCoordinates <$> [(x1,y1), (x2,y2),  (x2 + len, y2), (x1 + len,y1) ]
 
 drawOutline :: Shape -> Picture
 drawOutline (Square (x, y) size _) =
@@ -36,8 +44,12 @@ drawOutline (Square (x, y) size _) =
     $ Line
     $ translateCoordinates
         <$> [(x, y), (x + size, y), (x + size, y + size), (x, y + size), (x, y)]
+
+
 drawOutline (Triangle a b c _) =
   Scale 1.1 1.1 $ Color black $ Line $ translateCoordinates <$> [a, b, c, a]
+
+drawOutline (Parallel {}) = blank
 
 data Dir
   = L
@@ -64,3 +76,6 @@ move d (Triangle p1 p2 p3 clr) =
     (p2 `add` moveOffset d)
     (p3 `add` moveOffset d)
     clr
+
+move d (Parallel a b len clr) =
+  Parallel (a `add` moveOffset d) (b `add` moveOffset d) len clr
