75e9d0d102956c804278416909bc4217d8998ce3
salvat <tobias@avlas.cz>
2026-04-04 14:25:24 +0200

    Add comments to the Constants file

--- hw/salva1/app/Constants.hs
+++ hw/salva1/app/Constants.hs
@@ -1,7 +1,9 @@
 module Constants where
 
+-- defines a size of a unit in pixels
 unitSize = 200 :: Int
 
+-- defines the window size in units
 windowWidth = 4 * unitSize :: Int
 
 windowHeight = 3 * unitSize :: Int
