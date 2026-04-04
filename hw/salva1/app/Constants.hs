ae8a2ba4e2ecb4fc8c478f85f6f3dad4c6c6b50a
salvat <tobias@avlas.cz>
2026-04-04 17:33:02 +0200

    Improve constants file

--- hw/salva1/app/Constants.hs
+++ hw/salva1/app/Constants.hs
@@ -3,7 +3,10 @@ module Constants where
 -- defines a size of a unit in pixels
 unitSize = 200 :: Int
 
--- defines the window size in units
-windowWidth = 4 * unitSize :: Int
+unitWidth = 4 :: Int
+unitHeight = 3 :: Int
 
-windowHeight = 3 * unitSize :: Int
+-- defines the window size in units
+windowWidth = unitWidth * unitSize :: Int
+
+windowHeight = unitHeight * unitSize :: Int
