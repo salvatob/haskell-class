f79dc628a0fdfa76200f59f30d09fa855376b53f
salvat <tobias@avlas.cz>
2026-05-03 00:52:28 +0200

    Split hw2 into two parts

---
+++ hw/salva2/app/Lexer.hs
@@ -0,0 +1,101 @@
+{-# LANGUAGE TypeFamilies #-}
+module Lexer where
+
+import Control.Monad (void)
+import Data.Bool (bool)
+import Data.List (intercalate)
+import Data.List.NonEmpty (NonEmpty(..))
+import Data.Void (Void)
+import Text.Megaparsec
+import Text.Megaparsec.Char
+
+{- | A data type for tokens. `TBlanks` stores the size of the blank space,
+ - because we need it to measure the indentation width. -}
+data Tok
+  = TInt Int
+  | TNewLine
+  | TBlanks Int
+  | TOp Char -- operators + - * / % < > 
+  | TLeftPar
+  | TRightPar
+  | TColon
+  | TChar Char
+  | TIdentifier String -- represents a variable or function name
+  | TIf
+  | TElse
+  | TFuncDef
+  | TPass
+  deriving (Show, Eq, Ord)
+
+-- | We use this function to show the tokens in error messages from the parser,
+-- such as "unexpected TNewLine". If you want better error messages (such as
+-- "unexpected line ending" :D ), modify this function first.
+showTok :: Tok -> String
+showTok = show
+
+data T a = T
+  { strT :: String -- ^ discards the data and returns the original string
+  , unT :: !a -- ^ discards the wrap and returns the data
+  } deriving (Show)
+
+isNewLine (T _ TNewLine) = True
+isNewLine _ = False
+
+-- | The tokenizer eats normal Strings
+type Tokenizer = Parsec Void String
+
+parseOp :: Tokenizer (T Tok)
+parseOp = do
+  op <- oneOf "+-*/%<>"
+  return $ T [op] (TOp op)
+
+parseNl :: Tokenizer (T Tok)
+parseNl = T "\n" TNewLine <$ char '\n'
+
+parse1 :: Tok -> Char -> Tokenizer (T Tok)
+parse1 t c = T [c] t <$ char c
+
+parseChar :: Tokenizer (T Tok)
+parseChar = do
+  char '\''
+  c <- latin1Char
+  char '\''
+  return $ T [c] (TChar c)
+
+parseIdentifier :: Tokenizer (T Tok)
+parseIdentifier = do
+  word <- some letterChar
+  return $ case word of
+    "if" ->  T word TIf
+    "else" ->  T word TElse
+    "def" ->  T word TFuncDef
+    "pass" ->  T word TPass
+    _ ->  T word (TIdentifier word)
+
+
+-- | This parses out all tokens (you might want to extend the token count)
+tok :: Tokenizer (T Tok)
+tok =
+  choice
+    [ try ((T <$> id <*> TInt . read) <$> some digitChar)
+    , (T <$> id <*> TBlanks . length) <$> some (char ' ')
+    , parse1 TNewLine '\n'
+    , parse1 TLeftPar '('
+    , parse1 TRightPar ')'
+    , parse1 TColon ':'
+    , parseOp
+    , try parseChar
+    , parseIdentifier
+    ]
+
+toks :: Tokenizer [T Tok]
+toks = many tok
+
+-- | A nice wrapper for the token stream (we'll need to make instances on this,
+-- so we want to have a type tag, not just a list alias).
+newtype TokStream = TokStream
+  { unTokStream :: [T Tok]
+  } deriving (Show)
+
+-- | This runs the tokenizer
+tokenize = runParser (TokStream <$> toks <* eof)
--- hw/salva2/app/Main.hs
+++ hw/salva2/app/Main.hs
@@ -1,5 +1,14 @@
+{-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
 module Main where
-import Parser (main)
+import Lexer
+import Parser
+import Text.Megaparsec
 
+
+-- | a bit of demonstration
 main :: IO ()
-main = Parser.main
+main = do
+  let input = "'P'"
+  let tokens = tokenize "input.txt" input
+  print tokens
+
