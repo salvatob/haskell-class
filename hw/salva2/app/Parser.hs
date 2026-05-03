9e6b670924bce7b58e0162706f411fc347f6b706
salvat <tobias@avlas.cz>
2026-05-03 18:05:32 +0200

    Move the lexer TokStream code to the Lexer file

--- hw/salva2/app/Parser.hs
+++ hw/salva2/app/Parser.hs
@@ -3,9 +3,6 @@
 module Parser where
 
 import Control.Monad (void)
-import Data.Bool (bool)
-import Data.List (intercalate)
-import Data.List.NonEmpty (NonEmpty(..))
 import Data.Void (Void)
 import Lexer
 import Text.Megaparsec
@@ -127,67 +124,3 @@ pairLine = (,) <$> pLexeme pInt <*> pLexeme pInt <* pLexeme (single TNewLine)
 
 -- | parse lots of integer pairs, each on a single line
 parsePairs = runParser (blanks *> many pairLine <* eof)
-
--- | This is a megaparsec Stream instance for our `TokStream`, which works as
--- an adapter between our lists of labeled tokens and megaparsec. Essentially,
--- it tells megaparsec how to consume the TokStream. Similar instances exist
--- for String, Text, ByteString, and other parser-input types.
---
--- Essentially, megaparsec is able to parse anything as long as it has the
--- Stream instance defined.
-instance Stream TokStream where
-  type Token TokStream = Tok -- token type that the parsing function is interested in
-  type Tokens TokStream = [Tok] -- type for "several tokens"
-  tokenToChunk _ = (: []) -- some conversion functions
-  tokensToChunk _ = id
-  chunkToTokens _ = id
-  chunkLength _ = length
-  chunkEmpty _ = null
-  take1_ (TokStream (x:xs)) = Just (unT x, TokStream xs) -- extracts a token
-  take1_ _ = Nothing
-  takeN_ n (TokStream l@(_:_)) = Just (map unT $ take n l, TokStream $ drop n l) -- extracts a chunk
-  takeN_ _ _ = Nothing
-  takeWhile_ f (TokStream l) =
-    (map unT $ takeWhile (f . unT) l, TokStream $ dropWhile (f . unT) l)
-
--- | THIS BELOW you don't usually want to read.
---
--- This is the "rest" of the TokStream instances that is required for
--- megaparsec to be able to reconstruct good error messages from our TokStream.
---
--- In particular, whole the `Stream` instance tells megaparsec how to get
--- values out of the stream, the VisualStream tells it how to show the tokens
--- to the user in case some token is e.g. known to be expected but missing...
-instance VisualStream TokStream where
-  showTokens _ (a :| b) = intercalate ", " $ map showTok (a : b)
-
--- | ...and the TraversableStream instance tells it how to walk the token
--- stream in such a way that reconstructing a good "example source" of where
--- the error has occured is easy.
-instance TraversableStream TokStream where
-  reachOffset o pst =
-    let (reachtoks, resttoks) =
-          splitAt (o - pstateOffset pst) . unTokStream $ pstateInput pst
-        (rln, rst) = break isNewLine (reverse reachtoks)
-        linesFinished = length . filter isNewLine $ rst
-        sameLine = linesFinished == 0
-        line = unLine (reverse rln)
-        rest = unLine (fst $ break isNewLine resttoks)
-        unLine = unTab . concatMap strT
-        unTab "" = ""
-        unTab ('\t':cs) = replicate (unPos $ pstateTabWidth pst) ' ' ++ unTab cs
-        unTab (c:cs) = c : unTab cs
-        unempty "" = "<empty line>"
-        unempty a = a
-        pfx = bool id (pstateLinePrefix pst ++) sameLine
-        sp = pstateSourcePos pst
-        col = length line + bool 1 (unPos $ sourceColumn sp) sameLine
-        row = unPos (sourceLine sp) + linesFinished
-     in ( Just . unempty $ pfx line ++ rest
-        , pst
-            { pstateInput = TokStream resttoks
-            , pstateOffset = o
-            , pstateSourcePos =
-                sp {sourceLine = mkPos row, sourceColumn = mkPos col}
-            , pstateLinePrefix = pfx line
-            })
