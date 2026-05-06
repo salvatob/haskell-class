c8155f114a73dc3e049b4d6f7fc2d5a996baf94e
salvat <tobias@avlas.cz>
2026-05-06 22:27:13 +0200

    add helper to the stack state

--- hw/salva2/app/StackState.hs
+++ hw/salva2/app/StackState.hs
@@ -23,6 +23,29 @@ pop = do
         return (Just x)
       [] -> return Nothing
 
+peek :: StackOp (Maybe Int)
+peek = do
+  items <- get
+  return $ case items of
+    []    ->  Nothing
+    (x:_) -> Just x
+
+-- | pop until the top item is equal to 'width'
+-- | if such item is present returns the number of popped items, else returns Nothing 
+popToWidth :: Int -> StackOp (Maybe Int)
+popToWidth w = do
+  lastIt <- peek
+  case lastIt of
+    Nothing -> return Nothing
+    Just level -> case compare level w of
+      GT ->   do
+        pop
+        wid <- popToWidth w
+        return $ Just (+1)  <*> wid
+        
+      EQ -> return (Just 0)
+      LT -> return Nothing
+
 
 testStack :: Stack -> IO ()
 testStack initialStack = do
