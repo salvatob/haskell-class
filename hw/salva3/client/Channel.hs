2363261818f59844c882a7a6fb182c44be3fc9b7
salvat <tobias@avlas.cz>
2026-07-01 19:09:02 +0200

    Defince an inital model for the clinet-server protocol architecture from the client side

--- hw/salva3/client/Channel.hs
+++ hw/salva3/client/Channel.hs
@@ -1,5 +1,36 @@
+{- cabal:
+    build-depends: base, network, containers
+-}
 module Channel where
 
+import Control.Concurrent
+import Network.Socket
+import System.IO
+import Control.Monad
 
-updateFromServer :: IO (Maybe String)
-updateFromServer = return Nothing
+import Editor
+
+type SharedWorld = MVar (Maybe ServerInfo)
+
+
+parseServerWorld :: String -> ServerInfo
+parseServerWorld = read
+
+
+
+readerThread :: Handle -> SharedWorld -> IO ()
+readerThread h shared = return ()
+-- readerThread h shared = forever $ do
+--     line <- hGetLine h
+--     case parseServerWorld line of
+--         Nothing -> pure ()
+--         Just w  -> do
+--             _ <- tryTakeMVar shared
+--             putMVar shared (Just w)
+
+
+updatesFromServer :: SharedWorld -> IO (Maybe ServerInfo)
+updatesFromServer shared = do
+    modifyMVar shared $ \m -> do
+        pure (Nothing, m)
+
--- hw/salva3/client/Client.hs
+++ hw/salva3/client/Client.hs
@@ -1,29 +1,50 @@
-
+module Main where
 {- cabal:
 build-depends: base, network
 -}
-module Main where
 
 import Control.Concurrent
 import Network.Socket
 import System.IO
 
-main =
-  withSocketsDo $ do
+import Channel
+import GameLoop
+
+-- main =
+--   withSocketsDo $ do
+--     sock <- socket AF_INET Stream 0
+--     addr <-
+--       addrAddress . head
+--         <$> getAddrInfo (Just defaultHints) (Just "127.0.0.1") (Just "10042")
+--     connect sock addr
+--     h <- socketToHandle sock ReadWriteMode
+--     hSetBuffering h NoBuffering
+--     hPutStrLn h "Cover [(0,0,E), (0,1,W)]"
+--     response <- hGetLine h
+--     putStrLn $ "Received a line: " ++ response
+--     hPutStrLn h "Poll"
+--     threadDelay 1000000
+--     response <- hGetLine h
+--     putStrLn $ "Received another line: " ++ response
+--     hPutStrLn h "Quit"
+--     threadDelay 100000
+--     hClose h
+
+main = withSocketsDo $ do
     sock <- socket AF_INET Stream 0
-    addr <-
-      addrAddress . head
-        <$> getAddrInfo (Just defaultHints) (Just "127.0.0.1") (Just "10042")
+
+    addr <- addrAddress . head
+        <$> getAddrInfo (Just defaultHints)
+                        (Just "127.0.0.1")
+                        (Just "10042")
+
     connect sock addr
     h <- socketToHandle sock ReadWriteMode
     hSetBuffering h NoBuffering
-    hPutStrLn h "Cover [(0,0,E), (0,1,W)]"
-    response <- hGetLine h
-    putStrLn $ "Received a line: " ++ response
-    hPutStrLn h "Poll"
-    threadDelay 1000000
-    response <- hGetLine h
-    putStrLn $ "Received another line: " ++ response
-    hPutStrLn h "Quit"
-    threadDelay 100000
-    hClose h
+
+    shared <- newMVar Nothing
+
+    _ <- forkIO $ readerThread h shared
+
+    
+    runGame shared
